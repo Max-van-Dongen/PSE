557c27ed5ad2f85d4903ebe4742afc129b2f2b99
martint219 <121238038+martint219@users.noreply.github.com>
2023-06-16 21:36:57 +0200

    Verbeterde Afstandssensoren klasse

---
+++ Afstandssensoren/main_voorbeeld.ino
@@ -0,0 +1,18 @@
+#include <Wire.h>
+#include <Zumo32U4.h>
+#include "Afstandssensoren.h"
+
+Zumo32U4ButtonA buttonA;
+Afstandssensoren prox;
+
+void setup()
+{
+  zumo.Initialiseren(); //initialiseert de afstandssensoren
+  buttonA.waitForButton(); //zodra A knop wordt gedrukt start de DetectieTest
+  delay(1000); //1 seconde wachten met starten DetectieTest na drukken knop
+}
+
+void loop()
+{
+ prox.DetectieTest(); //roept de DetectieTest methode uit voor het testen en configureren van de afstandssensoren
+}
